a3f96a7066a771891e86c5f9ad0b0b3f38fa339f
chertenok <ktoto47913@gmail.com>
2023-09-05 22:12:34 +0300

    Initial Commit

---
+++ HomeWork 3.01/ViewControllers/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  HomeWork 3.01
+//
+//  Created by Илья on 05.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
